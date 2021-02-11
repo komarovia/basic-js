09b1b36dad3749f58ad1d5605b397257d233e58e
komarovia <komarovia@gmail.com>
2021-02-11 16:56:26 +0300

    feat: completed Count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let newArray = [];
+  if (backyard.length === 0) {return 0}
+			else {for (let i = 0; i < backyard.length; i++){
+						newArray = newArray + backyard[i].slice([0]) + ",";
+					}
+
+			newArray = newArray.split(",");
+			let someUsers = newArray.filter(item => item === '^^');
+  return someUsers.length;}
 };
