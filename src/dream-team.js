9eacec006771a66fe3fe533e1693fd2cac242bdf
komarovia <komarovia@gmail.com>
2021-02-14 19:01:18 +0300

    feat: completed Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	let a = "";
+	let b = "";
+  if (Array.isArray(members) === true) {
+	members.map(item => {
+	if (typeof item	=== "string") {b = item.replace(/\s/g, ''); a = a + b.split('', 1)} else {false};
+	});
+	if (a === "") {return false}
+	else {a = a.toUpperCase();
+		a = a.split('');
+	a = a.sort();
+	a = a.join('');
+	return a}}
+  else {return false}
 };
