ed63fb58bdd4994ffc136548f5af5ae75e86032f
komarovia <komarovia@gmail.com>
2021-02-24 23:06:57 +0300

    feat: comleted simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+	arr: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+  return this.arr.length;
+	},
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+	this.arr.push(`( ${value} )`);
+	return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+		if (position < 0 || position > this.arr.length) {
+			this._delChain();
+			throw new Error();
+		}
+	else {this.arr.splice(position - 1, 1);
+	return this;}
+},
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+this.arr.reverse();
+	return this;
+},
+finishChain() {
+	const finChain = this._returnChain();
+	this._delChain();
+	return finChain;
+},
+_delChain() {
+	this.arr.length = 0;
+},
+_returnChain() {
+	return this.arr.join('~~');
+},
 };
 
 module.exports = chainMaker;
