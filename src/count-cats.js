67cb0ac1194a8a87656e0e35c0f45d76049f7412
komarovia <komarovia@gmail.com>
2021-02-25 23:52:28 +0300

    fix: task count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,10 +4,10 @@ module.exports = function countCats(backyard) {
   let newArray = [];
   if (backyard.length === 0) {return 0}
 			else {for (let i = 0; i < backyard.length; i++){
-						newArray = newArray + backyard[i].slice([0]) + ",";
+						newArray += backyard[i].slice() + ",";
 					}
 
 			newArray = newArray.split(",");
-			let someUsers = newArray.filter(item => item === '^^');
-  return someUsers.length;}
+			newArray = newArray.filter(item => item === '^^');
+  return newArray.length;}
 };
