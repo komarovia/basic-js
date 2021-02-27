587a084c6bd2febe5fde164400e96e81fb1c9a9e
komarovia <komarovia@gmail.com>
2021-02-27 17:58:51 +0300

    feat: completed hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  const step = 2 ** disksNumber - 1;
+  const time = Math.floor(3600 / turnsSpeed * step);
+  const obj = {turns: step, seconds: time};
+  return obj;
+ };
